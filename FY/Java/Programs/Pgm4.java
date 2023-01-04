02a499fbf32382917e6077bb4e682da3db5e098c
Omkar Lohar <omkarlohar91@gmail.com>
2023-01-04 20:30:30 +0530

    added output in Pgm4

--- FY/Java/Programs/Pgm4.java
+++ FY/Java/Programs/Pgm4.java
@@ -3,8 +3,10 @@ class ThisKeyword {
     int var1 = 10;
     int var2 = 20;
 
-    // If local variables and instance have same name we can use this keyword to
-    // access instance variable in method
+    /*
+     * If local variables and instance have same name we can use this keyword to
+     * access instance variable in method
+     */
     void print(int var1, int var2) {
         System.out.println("Local variable " + var1 + " " + var2);
         System.out.println("Instance variable " + this.var1 + " " + this.var2);
@@ -15,3 +17,10 @@ class ThisKeyword {
     }
 
 }
+
+/*
+ * Output :
+ * Local variable 100 200
+ * Instance variable 10 20
+ * 
+ */
