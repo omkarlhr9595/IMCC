2c0687c887a5a0fe74014d1ca0aada111d773bb3
Omkar Lohar <omkarlohar91@gmail.com>
2023-01-07 23:00:47 +0530

    nothing 1.4

--- FY/Java/Programs/Pgm8.java
+++ FY/Java/Programs/Pgm8.java
@@ -2,6 +2,7 @@ class ConstructorOverloading {
 
     int num1, num2, num3;
 
+    // Constructor with 2 parameters
     ConstructorOverloading(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
@@ -9,6 +10,7 @@ class ConstructorOverloading {
         System.out.println(this.num1 + " " + this.num2);
     }
 
+    // Constructor with 2 parameters
     ConstructorOverloading(int num1, int num2, int num3) {
         this.num1 = num1;
         this.num2 = num2;
--- FY/Java/Programs/Pgm9.java
+++ FY/Java/Programs/Pgm9.java
@@ -1,3 +1,15 @@
-class Pgm9 {
+class OperatorOverloading {
+
+    public static void main(String[] args) {
+
+        // In this statement + is overloading it self as a concatenation operator
+        System.out.println("Value of float is " + 3.14);
+
+    }
 
 }
+
+/*
+ * Output :
+ * Value of float is 3.14
+ */
