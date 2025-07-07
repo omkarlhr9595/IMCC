a94a98abced92d53f8dd0603ea591c568d1cfb47
omkarlhr9595 <omkarlohar91@gmail.com>
2025-07-07 09:15:14 +0530

    Add operators.dart to demonstrate basic arithmetic operations

---
+++ Flutter 2025/Div-B/7-7/operators.dart
@@ -0,0 +1,12 @@
+void main(){
+  int a = 15;
+  int b = 2;
+  int sum = a + b;
+  int difference = a - b;
+  int product = a * b;
+  double division = a / b; 
+  print("The sum of $a and $b is $sum");
+  print("The difference of $a and $b is $difference");
+  print("The product of $a and $b is $product");
+  print("The division of $a by $b is $division");
+}
