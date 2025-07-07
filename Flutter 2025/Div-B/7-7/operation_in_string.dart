c0adfcad3bbf9725e15102261aab042812756b6a
omkarlhr9595 <omkarlohar91@gmail.com>
2025-07-07 09:22:01 +0530

    Add operation_in_string.dart to demonstrate basic arithmetic operations and string interpolation

---
+++ Flutter 2025/Div-B/7-7/operation_in_string.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("Addition of 10 and 20 is ${10+20}");
+  print("Subtraction of 20 from 30 is ${30-20}");
+  print("Multiplication of 5 and 6 is ${5*6}");
+  print("Division of 20 by 4 is ${20/4}");
+  print("Modulus of 10 and 3 is ${10%3}");
+  
+  // Using variables in string interpolation
+  int likes = 10;
+  print("I like Flutter ${likes+1} times");
+}
