abbe7ecc5274dc9cd009dde1a181143836728f1b
omkarlhr9595 <omkarlohar91@gmail.com>
2025-07-19 00:38:29 -0400

    feat: Add background image and layout to IdentityCardScreen

--- Flutter 2025/projects/identity_card/lib/screens/identity_card_screen.dart
+++ Flutter 2025/projects/identity_card/lib/screens/identity_card_screen.dart
@@ -12,7 +12,50 @@ class _IdentityCardScreenState extends State<IdentityCardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/id_card_.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: 70),
+              Text(
+                "COMPANY NAME",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "TAG LINE GOES HERE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/harry_potter.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(1000),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
