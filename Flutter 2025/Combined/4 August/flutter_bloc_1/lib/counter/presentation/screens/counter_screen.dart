b2109fea4eacbf9ca3a361a3c4d712557604d94a
omkarlhr9595 <omkarlohar91@gmail.com>
2025-08-06 10:03:13 +0530

    feat: implement counter functionality with increment and decrement widgets

--- Flutter 2025/Combined/4 August/flutter_bloc_1/lib/counter/presentation/screens/counter_screen.dart
+++ Flutter 2025/Combined/4 August/flutter_bloc_1/lib/counter/presentation/screens/counter_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_1/counter/presentation/widgets/counter_widget.dart';
+import 'package:flutter_bloc_1/counter/presentation/widgets/decrement_counter_widget.dart';
+import 'package:flutter_bloc_1/counter/presentation/widgets/increment_counter_widget.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -10,6 +13,23 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: const SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IncrementCounterWidget(),
+            SizedBox(height: 20),
+            CounterWidget(),
+            SizedBox(height: 20),
+            DecrementCounterWidget(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ Flutter 2025/Combined/4 August/flutter_bloc_1/lib/counter/presentation/widgets/decrement_counter_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_1/counter/presentation/bloc/counter_bloc.dart';
+
+class DecrementCounterWidget extends StatelessWidget {
+  const DecrementCounterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+    return ElevatedButton(
+      onPressed: () {
+        counterBloc.add(CounterDecrementedEvent());
+      },
+      child: const Icon(Icons.remove),
+    );
+  }
+}
