874b8160c9bb94597553fa8b2728387ccf4bddba
omkarlhr9595 <omkarlohar91@gmail.com>
2025-08-13 23:16:44 +0530

    feat: integrate dynamic color support for adaptive theming

--- Flutter 2025/projects/the_movie_app/lib/app.dart
+++ Flutter 2025/projects/the_movie_app/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 import 'features/trending_movies/presentation/bloc/trending_bloc.dart';
 import 'features/trending_movies/presentation/screens/trending_screen.dart';
@@ -15,13 +16,41 @@ class App extends StatelessWidget {
       providers: <BlocProvider<dynamic>>[
         BlocProvider<TrendingBloc>(create: (_) => TrendingBloc(sl())..add(const TrendingRequestedEvent())),
       ],
-      child: MaterialApp(
-        theme: ThemeData(
-          colorSchemeSeed: const Color(0xFF6750A4),
-          useMaterial3: true,
-          textTheme: GoogleFonts.interTextTheme(),
-        ),
-        home: const TrendingScreen(),
+      child: DynamicColorBuilder(
+        builder: (lightDynamic, darkDynamic) {
+          ColorScheme lightScheme;
+          ColorScheme darkScheme;
+
+          if (lightDynamic != null && darkDynamic != null) {
+            lightScheme = lightDynamic.harmonized();
+            darkScheme = darkDynamic.harmonized();
+          } else {
+            const fallbackSeed = Color(0xFF6750A4);
+            lightScheme = ColorScheme.fromSeed(
+              seedColor: fallbackSeed,
+              brightness: Brightness.light,
+            );
+            darkScheme = ColorScheme.fromSeed(
+              seedColor: fallbackSeed,
+              brightness: Brightness.dark,
+            );
+          }
+
+          return MaterialApp(
+            theme: ThemeData(
+              colorScheme: lightScheme,
+              useMaterial3: true,
+              textTheme: GoogleFonts.interTextTheme(),
+            ),
+            darkTheme: ThemeData(
+              colorScheme: darkScheme,
+              useMaterial3: true,
+              textTheme: GoogleFonts.interTextTheme(),
+            ),
+            themeMode: ThemeMode.system,
+            home: const TrendingScreen(),
+          );
+        },
       ),
     );
   }
