fe2199547fd362acf8d904826c9e31ab7ce6d42c
omkarlhr9595 <omkarlohar91@gmail.com>
2025-08-21 10:00:36 +0530

    feat: load environment variables using dotenv in main function

--- Flutter 2025/projects/notes_crud_app/lib/main.dart
+++ Flutter 2025/projects/notes_crud_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:notes_crud_app/app.dart';
 import 'package:notes_crud_app/core/config/firebase_options.dart';
 
 void main() async {
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
