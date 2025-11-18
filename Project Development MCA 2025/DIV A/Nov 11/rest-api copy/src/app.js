a7d958e6b3afcf64bb4515fd1a10e55dde871f45
omkarlhr9595 <omkarlohar91@gmail.com>
2025-11-18 08:18:52 +0530

    feat: add new assignment HTML files and update project structure
    - Introduced new assignment HTML files with visual examples for Flutter projects.
    - Added a .zip file containing the assignment HTML for easier distribution.
    - Removed the outdated first_assignment.html file to streamline the assignments directory.
    - Included a .gitignore file for the new REST API project to exclude node_modules.
    - Created initial package.json and package-lock.json for the REST API project with Express.js and Mongoose dependencies.
    - Established basic server structure with app.js and index.js for handling requests.

---
+++ Project Development MCA 2025/DIV A/Nov 11/rest-api copy/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+// app.use("/api/users", userRoutes);
+// app.use("api/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.status(404).json({ message: "Hello World" });
+});
+
+app.get("/greet/:name", (req, res) => {
+  res.send("<h1>Hello " + req.params.name + "</h1>");
+});
+
+export default app;
